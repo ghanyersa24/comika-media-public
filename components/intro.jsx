a2c35b95522e01c3c5d3d68ceb1a07a260f6351e
rudiwawa <rudiwawa1980@gmail.com>
2021-06-09 20:04:14 +0700

    hero bisa

--- components/intro.jsx
+++ components/intro.jsx
@@ -1,17 +1,22 @@
-import Carousel from 'react-elastic-carousel'
+import SwipeableViews from 'react-swipeable-views'
+import { autoPlay } from 'react-swipeable-views-utils'
+// import Image from 'next/image'
 import { CMS_NAME } from '../lib/constants'
 
+export const Item = ({ url }) => (
+  <div className="w-full">
+    <img src={url} alt="" className="w-full object-cover" />
+  </div>
+)
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 export default function Intro() {
   return (
-    <section className="w-full h-1/2 bg-blue-300">
-      <Carousel itemsToShow={1}>
-        <div className="w-full bg-red-300 h-1/2">
-          1
-        </div>
-        <div className="w-full bg-gray-300 h-1/2">
-          2
-        </div>
-      </Carousel>
+    <section className="mb-4">
+      <AutoPlaySwipeableViews enableMouseEvents className="h-4/5">
+        <Item url="https://media-origin.kompas.tv/library/image/thumbnail/1604991035/KALIS1604991035.jpg" />
+        <Item url="https://media-origin.kompas.tv/library/image/thumbnail/1615547951/NOPEK_-_KOMIKA_ANGIN-ANGINAN_KOK_MASUK_FINAL1615547951.jpg" />
+      </AutoPlaySwipeableViews>
     </section>
   )
 }
