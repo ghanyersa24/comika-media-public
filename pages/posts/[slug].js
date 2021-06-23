b3e90965bed589b97b3eb02c418bc6c389b3be5f
rudiwawa <rudiwawa1980@gmail.com>
2021-06-23 18:01:46 +0700

    detail berhasil tampil

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -33,13 +33,13 @@ export default function Post({ post, morePosts, preview }) {
                   | Next.js Blog Example with
                   {CMS_NAME}
                 </title>
-                <meta property="og:image" content={post.ogImage.url} />
+                {/* <meta property="og:image" content={post.ogImage.url} /> */}
               </Head>
               <PostHeader
                 title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
+                coverImage={post.banner}
+                date={post.updatedAt}
+                // author={post.author}
               />
               <PostBody content={post.content} />
             </article>
@@ -76,13 +76,13 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const posts = await client.get(`${API_ENDPOINT_ARTICLE}/${['slug']}`)
-  return {
-    paths: posts.map((post) => ({
-      params: {
-        slug: post.slug,
-      },
-    })),
-    fallback: false,
-  }
+  const posts = await client.get(`${API_ENDPOINT_ARTICLE}`)
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
 }
