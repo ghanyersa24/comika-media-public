88fee90bdb2a625e85730f68a1526a60d7d47cee
rudiwawa <rudiwawa1980@gmail.com>
2021-07-10 18:55:33 +0700

    slicing done

--- components/card/subscribe-item.tsx
+++ components/card/subscribe-item.tsx
@@ -1,28 +1,38 @@
 import { ReactChild, ReactElement } from 'react'
+import { AiOutlineClockCircle } from 'react-icons/ai'
 
 type SubsribeItemProps = {
 className?: string,
 children: ReactChild,
 title: string,
 price: string,
+until: string,
+buttonText: string,
+headBgColor: string,
 }
 
 export const SubsribeItem = ({
-  className, children, title, price,
+  className, children, title, price, until, buttonText, headBgColor,
 }: SubsribeItemProps): ReactElement => {
   console.log('🚀 ~ file: subscribe-item.tsx ~ line 4 ~ SubsribeItem ~ SubsribeItem')
   return (
-    <div className={`bg-white border-2 rounded-lg border-gray-200 relative ${className}`}>
-      <div className="bg-gray-300  rounded-tl-lg rounded-tr-lg h-3 -m-0.5 " />
-      <div className="p-4">
-        <p className="text-3xl font-bold  text-blue-700">Satu Paham</p>
-        <p className="text-3xl font-medium mt-2  text-gray-700">Rp29.900</p>
-        <p className="text-lg font-medium mt-2  text-gray-500">Berlaku untuk 7 Hari</p>
-        <button type="button" className="mt-4 inline-flex items-center justify-center w-full bg-primary rounded hover:bg-primaryDark">
-          <p className="flex-1 h-full text-2xl leading-loose text-center text-white">Subscribe Harian</p>
+    <div className={`bg-white border-2 rounded-lg  mt-8 border-gray-200 relative ${className}`}>
+      <div className={`rounded-tl-lg rounded-tr-lg h-3 -m-0.5 ${headBgColor}`} />
+      <div className="p-8">
+        <p className="text-3xl font-bold  text-primary">{title}</p>
+        <p className="text-3xl font-medium mt-4  text-gray-700">
+          Rp
+          {price}
+        </p>
+        <p className="text-lg font-medium mt-4  text-gray-500">
+          <AiOutlineClockCircle className="inline mr-2" />
+          {until}
+        </p>
+        <button type="button" className="mt-8 inline-flex items-center justify-center w-full bg-primary rounded hover:bg-primaryDark">
+          <p className="flex-1 h-full text-2xl leading-loose text-center text-white">{buttonText}</p>
         </button>
+        {children}
       </div>
-      {children}
     </div>
   )
 }
