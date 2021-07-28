75c57a0a7afe23c8adad99ba998a425024d065c6
rudiwawa <rudiwawa1980@gmail.com>
2021-07-28 15:49:23 +0700

    search done

--- pages/search/[slug].tsx
+++ pages/search/[slug].tsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/destructuring-assignment */
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
-
+import { useRouter } from 'next/router'
 import React, { } from 'react'
 import ContainerPadding from '../../components/container-padding'
-import MorePosts from '../../components/more-posts'
 import { IntroDekstop, IntroMobile } from '../../components/intro'
 import { client } from '../../lib/clientRaw'
 import { API_ENDPOINT_LIST_ARTICLE } from '../../res/api-endpoint'
 import { Post } from '../../res/interface'
 import Layout from '../../components/layout'
 import SearchNavigation from '../../components/blog/navigation/search-navigation-mobile'
+
+import MorePosts from '../../components/more-posts'
 
 type Props= {
   searchArticles:Post[],
@@ -21,6 +22,8 @@ export default function Index(
     searchArticles, isMobile,
   }:Props,
 ): React.ReactNode {
+  const router = useRouter()
+  const { slug } = router.query
   return (
     <Layout isMobile={isMobile}>
       <Head>
@@ -35,7 +38,7 @@ export default function Index(
       ) : <IntroDekstop />}
 
       <ContainerPadding className="mt-8 md:mt-12 mb-24">
-        {searchArticles.length > 0 && <MorePosts posts={searchArticles} title="Result" description="Hasil pencarian" />}
+        {searchArticles.length > 0 && <MorePosts posts={searchArticles} title="Hasil Pencarian" description={`Kata kunci  "${slug}"`} />}
       </ContainerPadding>
     </Layout>
   )
