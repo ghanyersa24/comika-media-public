22ef06864128b7cffced2c0b08940d8faf32bcd9
rudiwawa <rudiwawa1980@gmail.com>
2021-09-14 16:36:54 +0700

    save dulu aja ya kan

--- pages/setting/add-address.tsx
+++ pages/setting/add-address.tsx
@@ -1,13 +1,34 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { ReactElement } from 'react'
 import {
-  Formik, Field, Form, FormikHelpers,
+  Formik, Field, Form, FormikHelpers, getFieldProps, useField, useFormikContext,
 } from 'formik'
+import useSWR from 'swr'
 import { address } from '../../res/interface'
 import TopNavbarWithBackButton from '../../components/navigation/top-navbar-with-back-button'
+import { client } from '../../lib/clientRaw'
+
+const City = (props) => {
+  const {
+    values: { provinceId },
+  } = useFormikContext()
+  const [field] = useField(props)
+  const { data: city, isValidating } = useSWR(`/store/ongkir/master-city?province=${provinceId}`, client.get)
+  return (
+    <>
+      <select {...props} {...field} disabled={isValidating}>
+        {city?.map((p) => (
+          <option value={p.city_id} key={p.city_id}>{`${p.type} ${p.city_name}`}</option>
+        ))}
+      </select>
+    </>
+  )
+}
 
 const App = ():ReactElement => {
   console.log('App -> App', App)
+  const { data: provinces, error, mutate } = useSWR('/store/ongkir/master-province', client.get)
+  console.log('province', provinces)
   return (
     <div className="pt-10">
       <TopNavbarWithBackButton title="Tambah Alamat Baru" />
@@ -51,11 +72,18 @@ const App = ():ReactElement => {
           <div className="py-4">
             <h2 className="mb-2 text-xl font-medium leading-relaxed text-primary">Alamat</h2>
 
-            <label htmlFor="province">Provinsi</label>
-            <Field id="province" name="province" />
+            <label htmlFor="provinceId">Provinsi</label>
+            <Field id="provinceId" name="provinceId" as="select" className="w-full">
+              {provinces?.map((p) => (
+                <option value={p.province_id} key={p.province_id} className="">{p.province}</option>
+              ))}
+            </Field>
 
-            <label htmlFor="city">Kabupaten atau Kota</label>
-            <Field id="city" name="city" />
+            <label htmlFor="cityId">Kabupaten atau Kota</label>
+            <City name="cityId" as="select" />
+
+            <label htmlFor="address">Alamat</label>
+            <Field name="address" />
 
             <label htmlFor="postalCode">Kode Pos</label>
             <Field id="postalCode" name="postalCode" />
