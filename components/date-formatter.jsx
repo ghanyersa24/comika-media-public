8c7baa9c3f4502ea150c50824a4b9076e03d83b9
rudiwawa <rudiwawa1980@gmail.com>
2021-09-26 17:43:06 +0700

    with relative date & fix responsive comment

--- components/date-formatter.jsx
+++ components/date-formatter.jsx
@@ -1,7 +1,17 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, formatDistance } from 'date-fns'
 import id from 'date-fns/locale/id'
 
 export default function DateFormatter({ dateString }) {
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'd LLLL yyyy', { locale: id })}</time>
+}
+export function DateFormatterRelative({ dateString }) {
+  const date = parseISO(dateString)
+  return (
+    <time dateTime={dateString}>
+      {
+    formatDistance(date, new Date(), { locale: id })
+}
+    </time>
+  )
 }
