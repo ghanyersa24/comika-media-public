74be71fa79786192afcc482796fbe9c7df33727b
rudiwawa <rudiwawa1980@gmail.com>
2021-11-16 12:00:23 +0700

    ubah ke h2

--- components/page/notification/notification-non-transaction.tsx
+++ components/page/notification/notification-non-transaction.tsx
@@ -9,7 +9,7 @@ type propsTypes = {
 
 const NotificationNonTransaction:FunctionComponent<propsTypes> = ({ notification }) => (
   <div className="max-w-screen-lg min-h-screen px-4 py-4 mt-16 prose bg-white md:mx-auto md:my-16 rounded-t-2xl lg:prose-lg">
-    <h1 className="font-bold">{notification.title}</h1>
+    <h2 className="font-bold">{notification.title}</h2>
     <img src={notification.img} alt="" />
     <div
       className={markdownStyles.markdown}
