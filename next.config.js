29ee9b8abb68ce09b808bded32aa859380cc8c23
rudiwawa <rudiwawa1980@gmail.com>
2021-11-19 21:15:34 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60 * 30,
     domains: ['miro.medium.com', 'service-comika.herokuapp.com', 'awsimages.detik.net.id', 'www.static-src.com', 'betterstudio.com',
       'cdn-production-thumbor-vidio.akamaized.net', 'localhost', 'i2.wp.com', 'www.herworld.co.id', 'pbs.twimg.com',
       '1.bp.blogspot.com', 'www.comikacomedy.club', 'comika.id', 'api.comika.mediauploads', 'lh3.googleusercontent.com', 'i.ytimg.com', 'babussalambuana.com', 'api.comika.media', 'i.graphicmama.com', 'via.placeholder.com', 'i.imgur.com',
